e7ec859e5abd8150c922b498683e8ebdf4864689
Pablo Helguera <phelguera33@gmail.com>
2021-04-11 11:10:50 -0300

    Se termino de ralizar la logica para que se muestre el spinner mientras se espera la respuesta de la api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Frase from './components/Frase';
-import {useState, useEffect} from 'react';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Frase from "./components/Frase";
+import { useState, useEffect } from "react";
+import Spinner from "./components/Spinner";
 
 function App() {
   //creo el state
   const [personaje, setPersonaje] = useState({});
+  const [cargando, setCargando] = useState(false);
 
   useEffect(() => {
     //logica a ejecutar
     consultarApi();
-  },[]);
+  }, []);
 
-  const consultarApi = async() => {
-    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+  const consultarApi = async () => {
+    setCargando(true);
+    const respuesta = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes"
+    );
     const resultado = await respuesta.json();
 
     console.log(respuesta);
@@ -22,15 +27,36 @@ function App() {
 
     //guardar el personaje en el state
     setPersonaje(resultado[0]);
-  }
+    setCargando(false);
+  };
+
+  //operador ternario (condicion) ? (que pasa si es verdadero) : (que pasa si es falso)
+  const mostrarComponente =
+    cargando === true ? (
+      <Spinner></Spinner>
+    ) : (
+      <Frase personaje={personaje}></Frase>
+    );
 
   return (
-    <section className='container text-center my-5'>
-      <article className='d-flex flex-column align-items-center'>
-      <img className='w-75' src={process.env.PUBLIC_URL+ 'logoSimpsons.png'} alt="logo de los simpsons"/>
-      <Button variant="primary" className='my-5' onClick={() => consultarApi()}>Obtener Frase</Button>
+    <section className="container text-center my-5">
+      <article className="d-flex flex-column align-items-center">
+        <img
+          className="w-75"
+          src={process.env.PUBLIC_URL + "logoSimpsons.png"}
+          alt="logo de los simpsons"
+        />
+        <Button
+          variant="primary"
+          className="my-5"
+          onClick={() => consultarApi()}
+        >
+          Obtener Frase
+        </Button>
       </article>
-      <Frase personaje={personaje}></Frase>
+      {
+        mostrarComponente
+      }
     </section>
   );
 }
